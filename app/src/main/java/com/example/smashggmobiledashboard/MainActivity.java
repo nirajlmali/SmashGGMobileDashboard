a02f9fe90a5fbd65f4d3a8c9c2623cad90223966
Niraj Mali <nirajlmali@gmail.com>
2020-05-06 22:44:11 -0700

    Just trying stuff out. Added a button and listener. This is the basic for sending a request.

--- app/src/main/java/com/example/smashggmobiledashboard/MainActivity.java
+++ app/src/main/java/com/example/smashggmobiledashboard/MainActivity.java
@@ -3,6 +3,16 @@ package com.example.smashggmobiledashboard;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+//API Tools
+//import java.net.URLEncoder;
+//import com.mashape.unirest.http.HttpResponse;
+//import com.mashape.unirest.http.JsonNode;
+//import com.mashape.unirest.http.Unirest;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +20,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(mainListener);
     }
+
+    private View.OnClickListener mainListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Log.d("Button Listener", "onClick: Pressed");
+            EditText et = (EditText)findViewById(R.id.editText);
+            et.setText("Hello");
+        }
+    };
 }
